2343040b13c5b0404887af222de2cdea8472afd4
aportav <andi_idfer@hotmail.com>
2019-07-24 16:45:57 -0600

    Implement user interface

--- app.js
+++ app.js
@@ -7,7 +7,22 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Add static folder to server
+app.use(express.static("public"));
 
+// Get our html file
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+
+app.post("/", (req, res) => {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    console.log(firstName + lastName + email);
+});
 // Listen on port 3000
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
